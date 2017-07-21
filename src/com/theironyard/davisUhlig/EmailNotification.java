3e8b2a7f23be227fb812df26c667d80be0ad2dd4
Davis Uhlig <duhlig@daviss-mbp.lan>
2017-07-21 11:31:08 -0400

    took this out of clone method

--- src/com/theironyard/davisUhlig/EmailNotification.java
+++ src/com/theironyard/davisUhlig/EmailNotification.java
@@ -60,6 +60,6 @@ public class EmailNotification extends Notification {
 
     @Override
     protected Object clone() throws CloneNotSupportedException {
-        return new EmailNotification(getSubject(), getBody(), this.getRecipient(), this.getSmtpProvider());
+        return new EmailNotification(getSubject(), getBody(), getRecipient(), getSmtpProvider());
     }
 }
